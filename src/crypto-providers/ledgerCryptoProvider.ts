d96e1c0dcc584f28f3ac69361888622c985b5958
Peter Benc <peter.benc1@gmail.com>
2020-10-20 16:31:36 +0200

    Sign pool reg with ledger

--- src/crypto-providers/ledgerCryptoProvider.ts
+++ src/crypto-providers/ledgerCryptoProvider.ts
@@ -18,6 +18,15 @@ import {
   _Certificate,
   TxCertificateKeys,
   _Withdrawal,
+  _StakepoolRegistrationCert,
+  _DelegationCert,
+  _StakingKeyDeregistrationCert,
+  _StakingKeyRegistrationCert,
+  _PoolRelay,
+  _SingleHostIPRelay,
+  _MultiHostNameRelay,
+  _SingleHostNameRelay,
+  TxRelayTypes,
 } from '../transaction/types'
 import {
   Address,
@@ -26,15 +35,15 @@ import {
   Network,
 } from '../types'
 import {
-  isDelegationCertificate,
-  isStakepoolRegistrationCertificate,
-  isStakingKeyDeregistrationCertificate,
-  isStakingKeyRegistrationCertificate,
-} from './guards'
-import {
   LedgerCertificate,
   LedgerInput,
+  LedgerMultiHostNameRelay,
   LedgerOutput,
+  LedgerPoolOwnerParams,
+  LedgerPoolParams,
+  LedgerRelayParams,
+  LedgerSingleHostIPRelay,
+  LedgerSingleHostNameRelay,
   LedgerWithdrawal,
   LedgerWitness,
 } from './ledgerTypes'
@@ -81,7 +90,7 @@ export const LedgerCryptoProvider: () => Promise<CryptoProvider> = async () => {
     coins: number,
     changeOutput: _AddressParameters,
   ): LedgerOutput => ({
-    addressTypeNibble: changeOutput.addressType, // TODO: get from address
+    addressTypeNibble: changeOutput.addressType,
     spendingPath: changeOutput.paymentPath,
     amountStr: `${coins}`,
     stakingPath: changeOutput.stakePath,
@@ -103,11 +112,9 @@ export const LedgerCryptoProvider: () => Promise<CryptoProvider> = async () => {
   }
 
   const prepareStakingKeyRegistrationCert = (
-    cert: _Certificate, stakeSigningFiles: HwSigningData[],
+    cert: _StakingKeyDeregistrationCert | _StakingKeyRegistrationCert,
+    stakeSigningFiles: HwSigningData[],
   ): LedgerCertificate => {
-    if (
-      !isStakingKeyRegistrationCertificate(cert) && !isStakingKeyDeregistrationCertificate(cert)
-    ) throw Error()
     const path = findSigningPath(cert.pubKeyHash, stakeSigningFiles)
     return {
       type: cert.type,
@@ -116,9 +123,8 @@ export const LedgerCryptoProvider: () => Promise<CryptoProvider> = async () => {
   }
 
   const prepareDelegationCert = (
-    cert: _Certificate, stakeSigningFiles: HwSigningData[],
+    cert: _DelegationCert, stakeSigningFiles: HwSigningData[],
   ): LedgerCertificate => {
-    if (!isDelegationCertificate(cert)) throw Error()
     const path = findSigningPath(cert.pubKeyHash, stakeSigningFiles)
     return {
       type: cert.type,
@@ -127,15 +133,83 @@ export const LedgerCryptoProvider: () => Promise<CryptoProvider> = async () => {
     }
   }
 
+  const preparePoolOwners = (
+    owners: Buffer[], stakeSigningFiles: HwSigningData[],
+  ): LedgerPoolOwnerParams[] => {
+    const poolOwners = owners.map((owner) => {
+      const path = findSigningPath(owner, stakeSigningFiles)
+      return path
+        ? { stakingPath: path }
+        : { stakingKeyHash: owner.toString('hex') }
+    })
+    const ownersWithPath = poolOwners.filter((owner) => owner.stakingPath)
+    if (ownersWithPath.length > 1) throw Error('OwnerMultipleTimesInTx')
+    return poolOwners
+  }
+
+  const prepareRelays = (relays: _PoolRelay[]): LedgerRelayParams[] => {
+    const SingleIPRelay = (
+      { portNumber, ipv4, ipv6 }: _SingleHostIPRelay,
+    ): LedgerSingleHostIPRelay => ({
+      portNumber, ipv4Hex: ipv4?.toString('hex'), ipv6Hex: ipv6?.toString('hex'),
+    })
+
+    const SingleNameRelay = (
+      { dnsName, portNumber }: _SingleHostNameRelay,
+    ): LedgerSingleHostNameRelay => ({
+      portNumber, dnsName,
+    })
+
+    const MultiNameRelay = (
+      { dnsName }: _MultiHostNameRelay,
+    ): LedgerMultiHostNameRelay => ({
+      dnsName,
+    })
+
+    const prepareRelay = (relay: _PoolRelay) => {
+      switch (relay.type) {
+        case TxRelayTypes.SINGLE_HOST_IP:
+          return SingleIPRelay(relay as _SingleHostIPRelay)
+        case TxRelayTypes.SINGLE_HOST_NAME:
+          return SingleNameRelay(relay as _SingleHostNameRelay)
+        case TxRelayTypes.MULTI_HOST_NAME:
+          return MultiNameRelay(relay as _MultiHostNameRelay)
+        default: throw Error('Unsupported relay type')
+      }
+    }
+
+    return relays.map((relay) => ({
+      type: relay.type,
+      params: prepareRelay(relay),
+    }))
+  }
+
   const prepareStakePoolRegistrationCert = (
-    cert: _Certificate, stakeSigningFiles: HwSigningData[],
+    cert: _StakepoolRegistrationCert, stakeSigningFiles: HwSigningData[],
   ): LedgerCertificate => {
-    if (!isStakepoolRegistrationCertificate(cert)) throw Error()
-    const path = findSigningPath(cert.ownerPubKeys[0], stakeSigningFiles)
-    // TODO: we need to iterate through the owner pubkeys
-    return { // TODO: proper pool reg cert
+    const owners = preparePoolOwners(cert.poolOwnersPubKeyHashes, stakeSigningFiles)
+    const poolRegistrationParams: LedgerPoolParams = {
+      poolKeyHashHex: cert.poolKeyHash.toString('hex'),
+      vrfKeyHashHex: cert.vrfPubKeyHash.toString('hex'),
+      pledgeStr: `${cert.pledge}`,
+      costStr: `${cert.cost}`,
+      margin: {
+        numeratorStr: `${cert.margin.numerator}`,
+        denominatorStr: `${cert.margin.denominator}`,
+      },
+      rewardAccountHex: cert.rewardAddress.toString('hex'),
+      poolOwners: owners,
+      relays: prepareRelays(cert.relays),
+      metadata: {
+        metadataUrl: cert.metadata.metadataUrl,
+        metadataHashHex: cert.metadata.metadataHash.toString('hex'),
+      },
+    }
+
+    return {
       type: cert.type,
-      path,
+      poolKeyHashHex: cert.poolKeyHash.toString('hex'),
+      poolRegistrationParams,
     }
   }
 
