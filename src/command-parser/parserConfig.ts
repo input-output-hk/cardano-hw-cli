83ee237fcd5923b14744f12acd12fa68e1b52462
David Tran Duc <david.tran.duc@vacuumlabs.com>
2020-10-20 10:52:48 +0200

    Update help descriptions

--- src/command-parser/parserConfig.ts
+++ src/command-parser/parserConfig.ts
@@ -12,24 +12,39 @@ const txSigningArgs = {
     dest: 'network',
     const: parseNetwork('MAINNET'),
     default: parseNetwork('MAINNET'),
+    help: 'Use the mainnet magic id.',
   },
-  '--testnet-magic': { nargs: '?', dest: 'network', type: (magic: string) => parseNetwork('TESTNET', magic) },
+  '--testnet-magic': {
+    nargs: '?',
+    dest: 'network',
+    type: (magic: string) => parseNetwork('TESTNET', magic),
+    help: 'Use the testnet [NETWORK] magic id.',
+  },
   '--tx-body-file': {
-    required: true, dest: 'txBodyFileData', type: (path: string) => parseTxBodyFile(path),
+    required: true,
+    dest: 'txBodyFileData',
+    type: (path: string) => parseTxBodyFile(path),
+    help: 'Input filepath of the TxBody.',
   },
   '--hw-signing-file': {
     dest: 'hwSigningFileData',
     required: true,
     action: 'append',
     type: (path: string) => parseHwSigningFile(path),
+    help: 'Input filepath of the hardware wallet signing file.',
   },
   '--change-output-key-file': {
     dest: 'changeOutputKeyFileData',
     action: 'append',
     default: [],
     type: (path: string) => parseHwSigningFile(path),
+    help: 'Input filepath of change output file.',
   },
-  '--out-file': { required: true, dest: 'outFile' },
+  '--out-file': {
+    required: true,
+    dest: 'outFile',
+    help: 'Output filepath of the Tx.',
+  },
 }
 
 export const parserConfig = {
@@ -39,26 +54,54 @@ export const parserConfig = {
   shelley: {
     address: {
       'key-gen': {
-        '--path': { required: true, type: (path: string) => parsePath(path) },
-        '--hw-signing-file': { required: true, dest: 'hwSigningFile' },
-        '--verification-key-file': { required: true, dest: 'verificationKeyFile' },
+        '--path': {
+          required: true,
+          type: (path: string) => parsePath(path),
+          help: 'Derivation path to the key to sign with.',
+        },
+        '--hw-signing-file': {
+          required: true,
+          dest: 'hwSigningFile',
+          help: 'Output filepath of the verification key.',
+        },
+        '--verification-key-file': {
+          required: true,
+          dest: 'verificationKeyFile',
+          help: 'Output filepath of the hardware wallet signing file.',
+        },
       },
       show: {
-        '--payment-path': { required: true, type: (path: string) => parsePath(path), dest: 'paymentPath' },
-        '--staking-path': { required: true, type: (path: string) => parsePath(path), dest: 'stakingPath' },
+        '--payment-path': { required: true,
+          type: (path: string) => parsePath(path),
+          dest: 'paymentPath',
+          help: 'Payment derivation path.',
+        },
+        '--staking-path': { required: true,
+          type: (path: string) => parsePath(path),
+          dest: 'stakingPath',
+          help: 'Stake derivation path.',
+        },
         '--address-file': {
           required: true,
           type: (path: string) => parseAddressFile(path),
           dest: 'address',
+          help: 'Input filepath of the address.',
         },
       },
     },
     key: {
       'verification-key': {
         '--hw-signing-file': {
-          required: true, dest: 'hwSigningFileData', type: (path: string) => parseHwSigningFile(path),
+          required: true,
+          dest: 'hwSigningFileData',
+          type: (path: string) => parseHwSigningFile(path),
+          help: 'Input filepath of the hardware wallet signing file.',
         },
-        '--verification-key-file': { required: true, dest: 'verificationKeyFile' },
+        '--verification-key-file': {
+          required: true,
+          dest: 'verificationKeyFile',
+          help: 'Output filepath of the verification key.',
+        },
       },
     },
     transaction: {
@@ -69,6 +112,7 @@ export const parserConfig = {
           dest: 'hwSigningFileData',
           required: true,
           type: (path: string) => parseHwSigningFile(path),
+          help: 'Output filepath of the verification key.',
         },
       },
     },
